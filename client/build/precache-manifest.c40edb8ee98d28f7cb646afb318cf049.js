914516afcc2673e2f2c34aa4397a7836882195a0
auj <auj@ecs.csus.edu>
2021-04-21 20:08:47 -0700

    heroku

---
+++ client/build/precache-manifest.c40edb8ee98d28f7cb646afb318cf049.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5a69805b7da28fedfcb6dec2c57494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94031290bc4a4cc49530",
+    "url": "/static/css/2.a9cb75db.chunk.css"
+  },
+  {
+    "revision": "93509f77733477373203",
+    "url": "/static/css/main.2a3c3383.chunk.css"
+  },
+  {
+    "revision": "94031290bc4a4cc49530",
+    "url": "/static/js/2.3d075f5d.chunk.js"
+  },
+  {
+    "revision": "3043e3e727d80f353557f855ab1ce92b",
+    "url": "/static/js/2.3d075f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93509f77733477373203",
+    "url": "/static/js/main.2cdabd9d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b6864b07f2f0590fdbb98f86243c1688",
+    "url": "/static/media/1.b6864b07.JPG"
+  },
+  {
+    "revision": "7322bbb0dc0533a471cf1a062a5b9325",
+    "url": "/static/media/2.7322bbb0.JPG"
+  },
+  {
+    "revision": "293b203f1991d8dd64a76c66b3a8e400",
+    "url": "/static/media/3.293b203f.JPG"
+  },
+  {
+    "revision": "53e08e9d2b58c781324255d277fd0594",
+    "url": "/static/media/4.53e08e9d.JPG"
+  },
+  {
+    "revision": "b234d2ce154e909d05d7aa2c9e7c5726",
+    "url": "/static/media/5.b234d2ce.JPG"
+  },
+  {
+    "revision": "f8a779007e651683d481168780f09bb7",
+    "url": "/static/media/6.f8a77900.JPG"
+  },
+  {
+    "revision": "a00739565b793e35343f927ec2918b4a",
+    "url": "/static/media/asianpeople.a0073956.png"
+  },
+  {
+    "revision": "3ef99a97611edf39036c138df0bddf16",
+    "url": "/static/media/sd_logo.3ef99a97.png"
+  },
+  {
+    "revision": "e7b0b91632ecc4c6b40d58f6ade634eb",
+    "url": "/static/media/temp_photos.e7b0b916.png"
+  },
+  {
+    "revision": "25b51614d2f9addfbce8997cec558394",
+    "url": "/static/media/temp_photos1.25b51614.png"
+  }
+]);
